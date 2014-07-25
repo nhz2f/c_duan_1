074da91538034432a83ce735f7069ac55482aea8
nhz2f <530159847@qq.com>
2014-07-25 02:37:44 -0700

    ok

---
+++ app/controllers/infos_controller.rb
@@ -0,0 +1,62 @@
+require 'net/http'
+require 'net/https'
+require 'rexml/document'
+include REXML
+
+class InfosController < ApplicationController
+  def index
+    @data = d('www.baidu.com', '52cb4735-5778-4c8f-9e90-ed2e05120cbd', 'FuX1qN/fBlt2YRvsNWB+Ccz+5lAEoDkA7lRU+bgQryk')
+  end
+
+private
+  def d(query, userId, passwd)
+    C_duan.new(query, userId, passwd).get_data
+  end
+end
+
+class C_duan
+  def  initialize(query, userId, passwd)
+    @uri = "/Bing/SearchWeb/v1/Web?Query=%27#{query}%27"
+    @url = 'api.datamarket.azure.com'
+    @userId = userId#'52cb4735-5778-4c8f-9e90-ed2e05120cbd'
+    @passwd = passwd#'FuX1qN/fBlt2YRvsNWB+Ccz+5lAEoDkA7lRU+bgQryk'
+  end
+
+  def get_data
+    data = {}
+    start.elements.each('feed/entry/content/m:properties'){|e|
+      a = {}
+      a['title'] = e.elements['d:Title'].text
+      a['des'] = e.elements['d:Description'].text
+      a['displayurl'] = e.elements['d:DisplayUrl'].text
+      a['url'] =  e.elements['d:Url'].text
+      data[e.elements['d:ID'].text] = a
+    }
+    data
+  end
+
+  def start
+    pre_start.start(){|http|
+      req = http_get.new(@uri)
+      req.basic_auth(@userId, @passwd)
+      res = http.request(req)
+      doc = Document::new(res.body)
+      doc
+    }
+  end
+
+  def pre_start
+    http = http_client.new(@url, 443)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    http
+  end
+
+  def http_client
+    Net::HTTP
+  end
+
+  def http_get
+    Net::HTTP::Get
+  end
+end
