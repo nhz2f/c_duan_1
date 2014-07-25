119bac3e0876aec4e67f5a8942beb0c5cba29a1b
nhz2f <530159847@qq.com>
2014-07-24 21:53:40 -0700

    侧段查询init

---
+++ db/migrate/20140725045010_create_infos.rb
@@ -0,0 +1,12 @@
+class CreateInfos < ActiveRecord::Migration
+  def change
+    create_table :infos do |t|
+      t.string :title
+      t.string :description
+      t.string :displayurl
+      t.string :url
+
+      t.timestamps
+    end
+  end
+end
